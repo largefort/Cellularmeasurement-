5088ebca5defecb63732a389193aebf367073878
jafetegill17 <106451813+largefort@users.noreply.github.com>
2024-02-16 22:11:37 +0000

    Create loading.js

---
+++ loading.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function () {
+    // Simulate loading time (replace with actual loading logic)
+    setTimeout(() => {
+        // Redirect to the main page after loading
+        window.location.replace("index.html");
+    }, 3000); // Adjust the timeout as needed
+});
